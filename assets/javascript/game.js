d79d1dd086be7aa892010757c12590811d3274a0
dissonance <ryanminor01u@yahoo.com>
2018-10-24 20:57:46 -0500

    red button functionality

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,48 +1,41 @@
 // safeguarding for writing jquery
-$(document).ready(function(){
+$(document).ready(function () {
 
-// starting variables for wins, losses and jewels
-var computerPick = "";
-var wins = 0;
-var losses = 0;
-var red = "";
-var blue = "";
-var yellow = "";
-var green = "";
-var userScore = 0;
+    // starting variables for wins, losses and jewels
+    var computerPick = "";
+    var wins = 0;
+    var losses = 0;
+    var red = "";
+    var blue = "";
+    var yellow = "";
+    var green = "";
+    var userScore = 0;
 
-// gamestart function for picking random value between 1 and 12 for each jewel and 19 and 120 for the 
-function gameStart() { 
-    red =  Math.floor(Math.random()*12)+1;
-    blue = Math.floor(Math.random()*12)+1;
-    yellow = Math.floor(Math.random()*12)+1;
-    green = Math.floor(Math.random()*12)+1;
-    computerPick = Math.floor(Math.random()* 101)+19;
-    $("#computerPick").text(computerPick);
-    $("#userScore").text(userScore);
-    // console logging to make sure random numbers are being chosen
-    console.log("red gem is " + red);
-    console.log("blue gem is " + blue);
-    console.log("green gem is " + green);
-    console.log("yellow gem is " + yellow);
-    console.log("computers pick is " + computerPick);
-};
+    // gamestart function for picking random value between 1 and 12 for each jewel and 19 and 120 for the 
+    function gameStart() {
+        red = Math.floor(Math.random() * 12) + 1;
+        blue = Math.floor(Math.random() * 12) + 1;
+        yellow = Math.floor(Math.random() * 12) + 1;
+        green = Math.floor(Math.random() * 12) + 1;
+        computerPick = Math.floor(Math.random() * 101) + 19;
+        $("#screen").html(computerPick);
+        $("#score-area").html(userScore);
+        // console logging to make sure random numbers are being chosen
+        console.log("red gem is " + red);
+        console.log("blue gem is " + blue);
+        console.log("green gem is " + green);
+        console.log("yellow gem is " + yellow);
+        console.log("computer pick is " + computerPick);
+    };
 
-
-
-
-gameStart ();
-
-
-
-
+// on click for all gem images
+    $("#red").on("click", function () {
+        var redClick = userScore += red;
+        $("#score-area").html(redClick);
+        console.log("red click is " + redClick);
+    });
 
 
 
-
-
-
-
-
-
+    gameStart();
 });
